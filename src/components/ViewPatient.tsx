3f20e79517db27bbf282e51768553bf71598e600
dmajor3 <dmajor@rezilienthealth.com>
2022-06-08 22:00:55 -0400

    Testing push to main

--- src/components/ViewPatient.tsx
+++ src/components/ViewPatient.tsx
@@ -1,15 +1,17 @@
-import { ReactElement } from 'react';
-import { Link } from 'react-router-dom';
-import { Box } from '@mui/material';
+import { ReactElement } from "react";
+import { Link } from "react-router-dom";
+import { Box } from "@mui/material";
 
 // Uncomment to use the mock api calls.
 // import * as api from '../mocks/api';
+
+console.log("Good luck!");
 
 const ViewPatient = (): ReactElement => {
   return (
     <Box>
       <h2>[View Patient Placeholder]</h2>
-      <Link to={'/patients'}>[List Patient Link Example]</Link>
+      <Link to={"/patients"}>[List Patient Link Example]</Link>
     </Box>
   );
 };
